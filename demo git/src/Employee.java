26032a0ef9cc75010901c663dd071870a06b4a05
2365217 <nalluribhargav8070@gmail.com>
2019-05-30 16:47:48 +0530

    cheetha

---
+++ demo git/src/Employee.java
@@ -0,0 +1,25 @@
+
+public class Employee {
+private int empid;
+private String empname;
+public int getEmpid() {
+	return empid;
+}
+public void setEmpid(int empid) {
+	this.empid = empid;
+}
+public String getEmpname() {
+	return empname;
+}
+public void setEmpname(String empname) {
+	this.empname = empname;
+}
+public double getEmpsal() {
+	return empsal;
+}
+public void setEmpsal(double empsal) {
+	this.empsal = empsal;
+}
+private double empsal;
+
+}
